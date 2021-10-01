8121db7f92127fa874b48e8af5877210e4228eb4
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-10-01 19:35:53 -0300

    `as` statement examples

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,29 +1,6 @@
-// types
-// interfaces
+const input = document.getElementById('input') as HTMLInputElement
 
-interface IAnimal {
-  name: string;
-  type: "terrestre" | "aquático";
-  roar(intensity: number): void;
-}
-
-interface IFelino extends IAnimal {
-  nightSight: boolean;
-}
-
-const animal: IAnimal = {
-  name: "Elefante",
-  type: "terrestre",
-  roar: (intensity) => (console.log(`${intensity}dB`))
-};
-
-animal.roar(25)
-
-const felino: IFelino = {
-  name: "Leão",
-  type: "terrestre",
-  nightSight: true,
-  roar: function (intensity: number): void {
-    throw new Error("Function not implemented.");
-  }
-};
+input.addEventListener("input", (event) => {
+  const i = event.currentTarget as HTMLInputElement
+  console.log(i.value)
+});
