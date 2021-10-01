5686dd2f2e4c728ae3ee51d1b7ef443fc055b258
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2021-10-01 19:26:26 -0300

    Examples of interface

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,7 +1,29 @@
-console.log('Typescript')
+// types
+// interfaces
 
-function sum(a: number, b: number) {
-    return a + b
+interface IAnimal {
+  name: string;
+  type: "terrestre" | "aquático";
+  roar(intensity: number): void;
 }
 
-console.log(sum('s', 23))
+interface IFelino extends IAnimal {
+  nightSight: boolean;
+}
+
+const animal: IAnimal = {
+  name: "Elefante",
+  type: "terrestre",
+  roar: (intensity) => (console.log(`${intensity}dB`))
+};
+
+animal.roar(25)
+
+const felino: IFelino = {
+  name: "Leão",
+  type: "terrestre",
+  nightSight: true,
+  roar: function (intensity: number): void {
+    throw new Error("Function not implemented.");
+  }
+};
